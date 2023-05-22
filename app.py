ad42f96cba90c6940c27f8d09f8c53e3b1feb643
Alessandro <alessandro.negri@gmail.com>
2023-05-22 16:32:16 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -79,6 +79,23 @@ def send_welcome(message):
                  ("Hi there, I am EchoBot.\n"
                   "I am here to echo your kind words back to you."))
 
+@bot.message_handler(commands=['greet'])
+def greet(message):
+    bot.send_message(message.chat.id, "Hey how's it going?")
+
+
+def dict_definition(message):
+    request = message.text.split()
+    if len(request) < 2 or request[0].lower() not in "price":
+        return False
+    else:
+        return True
+
+
+@bot.message_handler(func=dict_definition)
+def vocab_def(message):
+    print(f"message: {message}")
+
 
 def make_app():
     bot.remove_webhook()
